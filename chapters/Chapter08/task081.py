318b10ff6a29e9624f4bd717fd62c5e0862e71d6
nitinvdubey89 <80169684+nitinvdubey89@users.noreply.github.com>
2022-11-11 23:20:01 +0400

    Delete chapters directory

--- chapters/Chapter08/task081.py
+++
@@ -1,6 +0,0 @@
-from jinja2 import Template
-import yaml
-
-template = Template(open("task081.j2").read())
-vars = yaml.safe_load(open('task081.yml','r'))
-print(template.render(vars))
--- chapters/Chapter08/task084.py
+++
@@ -1,6 +0,0 @@
-from jinja2 import Template
-import yaml
-
-template = Template(open("task084.j2").read())
-vars = yaml.safe_load(open('task084.yml','r'))
-print(template.render(vars))
