e43d7ab6f3dc8f1d7b6b65eb2b8acae25ce0c238
nitinvdubey89 <80169684+nitinvdubey89@users.noreply.github.com>
2022-11-11 23:20:23 +0400

    Delete lab-2 directory

--- lab-2/challenge-207/apply-netconf.py
+++
@@ -1,5 +0,0 @@
-from ncclient import manager
-
-router1_config = open("acl.xml").read()
-router1 = manager.connect(host='192.168.255.51', username='expert', password='1234QWer!', hostkey_verify=False)
-router1.edit_config(router1_config, target="running")
